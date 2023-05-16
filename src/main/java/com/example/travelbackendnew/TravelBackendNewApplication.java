7e47216b5f0f6cf29c482ee8e81aa62a6f1103d8
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-05-16 22:09:41 +0200

    Initial project

---
+++ src/main/java/com/example/travelbackendnew/TravelBackendNewApplication.java
@@ -0,0 +1,14 @@
+package com.example.travelbackendnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBackendNewApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TravelBackendNewApplication.class, args);
+    }
+
+}
